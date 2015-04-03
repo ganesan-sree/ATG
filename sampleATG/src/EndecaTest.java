a7fe32e088f0b9f5fbb516b28dc7e22d2864cb1e
ganesan-sree <ganesan186@gmail.com>
2015-04-03 14:42:36 +0530

    endeca test chnages

--- sampleATG/src/EndecaTest.java
+++ sampleATG/src/EndecaTest.java
@@ -1,7 +1,3 @@
-
-
-
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -25,15 +21,10 @@ import com.endeca.navigation.PropertyMap;
 import com.endeca.navigation.UrlENEQuery;
 import com.endeca.navigation.UrlENEQueryParseException;
 
-
-
-
 public class EndecaTest 
 {
-	private final String mClassName = this.getClass().toString();
-	static int count = 1;
-
 	
+	static int count = 1;
 	public static void main(String[] args){
 		try {
 			long currenttime = System.currentTimeMillis();
@@ -41,13 +32,10 @@ public class EndecaTest
 			long endTime = System.currentTimeMillis() - currenttime;
 			System.out.println("time taken   ="+String.valueOf(endTime));
 		} catch (UrlENEQueryParseException e) {
-			// TODO Auto-generated catch block
+				e.printStackTrace();
+		} catch (InitializationException e) {		
 			e.printStackTrace();
-		} catch (InitializationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ContentException e) {
-			// TODO Auto-generated catch block
+		} catch (ContentException e) {		
 			e.printStackTrace();
 		}
 	}
@@ -77,9 +65,7 @@ public class EndecaTest
 		nequery.setNo("0");
 		
 		//N=0&Ne=14&Ntk=Refine&Ntt=baby&Ntx=mode+matchallpartial&Nty=1
-		
-		/*Start code CR03228 for setting user profiles to differentiate RxTied and Non-RxTied users*/
-		
+						
 		contentResults = getQueryResults(nequery,true);
 
 		contentSet.put("Result", contentResults);
@@ -102,34 +88,16 @@ public class EndecaTest
 				ERec erec = erecs.next();
 				PropertyMap props = erec.getProperties();
 				Collection val = props.getValues("p_PickupStoreId");
-				//String value = (String)props.get("p_PickupStoreId");
-				getStoreId(val);
-				
-				
+				System.out.println(props);
 			}
 		}
 		long end = System.currentTimeMillis() - start;
 		System.out.println("time taken inside loop "+String.valueOf(end));
 	}
 
-	static void printProdId(String value){
-		System.out.println(value);
-		
-	}
+
 	
-	static void getStoreId(Collection value){
-		long start = System.currentTimeMillis();
-		if(null != value){
-			
-			if(value.contains("4514")){
-				System.out.println("Store id 4514 is present from the endeca list.");
-			}
-			
-			
-		}
-		long end = System.currentTimeMillis() - start;
-		System.out.println("Time taken for each sku -->"+end);
-	}
+
 	/**
 	 * 
 	 * This method gets the ENEQuery as input parameter and establishes a connection to Endeca engine
@@ -141,20 +109,10 @@ public class EndecaTest
 	 */
 
 	public static ENEContentResults getQueryResults (ENEQuery  nequery,boolean pNavAllRefinements) throws  InitializationException, ContentException 
-	{
-
-		String methodName="getQueryResults()";
-		
-
-		
-		
-		ENEConnection connection = new HttpENEConnection("10.242.245.144",
-				"15000");
-
+	{	
+		ENEConnection connection = new HttpENEConnection("10.242.245.144","15000");
 		ENEContentResults resultSet = null;
-
 		
-
 		if (connection != null) {
 
 			nequery.setNavAllRefinements(true);
